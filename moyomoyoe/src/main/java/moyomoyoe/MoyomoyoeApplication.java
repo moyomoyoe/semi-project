2ff55d53f4aff5cd034f25890de2b2f1e7f55f5d
[eung3371] <eung33714752@gmail.com>
2024-09-13 17:16:52 +0900

    [main] 기본 프로젝트 생성

---
+++ moyomoyoe/src/main/java/moyomoyoe/MoyomoyoeApplication.java
@@ -0,0 +1,13 @@
+package moyomoyoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyomoyoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyomoyoeApplication.class, args);
+    }
+
+}
