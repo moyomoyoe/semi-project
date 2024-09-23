8f023bd4c7b31f45cd190a7b3a5c75954563ecdd
Wondear <vytpals00@gmail.com>
2024-09-23 17:07:14 +0900

    [test] 통합중

--- moyomoyoe/src/main/java/moyomoyoe/MoyomoyoeApplication.java
+++ moyomoyoe/src/main/java/moyomoyoe/MoyomoyoeApplication.java
@@ -1,13 +1,26 @@
 package moyomoyoe;
 
+import moyomoyoe.member.user.model.service.DummyDataGenerator;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MoyomoyoeApplication {
+@SpringBootApplication(scanBasePackages = "moyomoyoe")
+@MapperScan(basePackages = "moyomoyoe", annotationClass = Mapper.class)
+public class MoyomoyoeApplication implements CommandLineRunner {
+
+    @Autowired
+    private DummyDataGenerator dummyDataGenerator;
 
     public static void main(String[] args) {
-        SpringApplication.run(MoyomoyoeApplication.class, args);
+        SpringApplication.run(moyomoyoe.MoyomoyoeApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        dummyDataGenerator.generate();
+    }
 }
