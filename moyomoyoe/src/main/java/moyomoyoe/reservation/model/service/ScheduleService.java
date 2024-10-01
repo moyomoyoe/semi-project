72dd3e3b30f27b0558f9bb99f97269841390e1bd
Wondear <vytpals00@gmail.com>
2024-10-01 10:26:18 +0900

    [feature] 예약이 있는 스케쥴/사업장을 삭제시 예약취소와 연계되도록

--- moyomoyoe/src/main/java/moyomoyoe/reservation/model/service/ScheduleService.java
+++ moyomoyoe/src/main/java/moyomoyoe/reservation/model/service/ScheduleService.java
@@ -1,5 +1,6 @@
 package moyomoyoe.reservation.model.service;
 
+import moyomoyoe.reservation.model.dao.ReservationMapper;
 import moyomoyoe.reservation.model.dto.ScheduleDTO;
 import moyomoyoe.reservation.model.dto.StoreDTO;
 import moyomoyoe.reservation.model.dao.ScheduleMapper;
@@ -12,11 +13,15 @@ import java.util.List;
 
 @Service
 public class ScheduleService {
-    moyomoyoe.reservation.model.dao.ScheduleMapper dao;
+    ScheduleMapper dao;
+    ReservationMapper resDao;
+
     @Autowired
-    public ScheduleService(ScheduleMapper dao) {
+    public ScheduleService(ScheduleMapper dao, ReservationMapper resDao) {
         this.dao = dao;
+        this.resDao = resDao;
     }
+
 
     public Integer FindUserStore(int code){
         return dao.findUserStore(code);
@@ -32,15 +37,6 @@ public class ScheduleService {
 
     @Transactional
     public void registSchedule(int code, List<ScheduleDTO> scheduleDTOS) {
-        // 새로운 일정 사라진 일정 반영,
-        //1. 해당 일정에 예약이 있는 지 확인함
-        // <쉬운방식 아무튼 예약 다 취소함.
-
-        //어려운 방식 기존의 일정테이블을 가져온 뒤,
-        // 삭제된 일정의 예약만 취소
-        //2. 남아있는지 확인을 어떻게 하는가? equals 오버라이딩
-        //그러면 필요한 것. 기존의 스케쥴과 보내준 스케쥴 리스트...
-
         //기존의 일정
         List<ScheduleDTO> resent = dao.getSchedule(code);
         //전달받은 매개변수에, 기존의 일정과 "같은" 일정이 있는지 확인, 없는 애들은 삭제조치,
@@ -64,13 +60,15 @@ public class ScheduleService {
         //삭제할 일정의 예약도 삭제해야함 -> 예약 취소랑 통합 후 재 테스트 필요
         for(int id : deletedSchedules){
             System.out.println(id+ "삭제로직 구비중");
+            List<Integer> resList = dao.getResBySchedule(id);
+                for(int rid : resList)
+                    resDao.deleteReservation(rid);
             dao.deleteScheduleId(id);
         }
         for(ScheduleDTO s:insertSchedules){
             System.out.println(s+"삽입로직 쿼리 준비중");
             dao.registSchedule(s);
         }
-
     }
 
     @Transactional
